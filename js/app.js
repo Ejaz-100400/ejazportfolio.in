aa012b3981c90a9300f2f5cf04c8407bdff1f410
Ejaz-100400 <76722208+Ejaz-100400@users.noreply.github.com>
2022-10-22 20:28:58 +0530

    removing comments of swiperjs 22oct

--- js/app.js
+++ js/app.js
@@ -120,35 +120,6 @@ $("#fadeout li:nth-child(1)").click(function(e) {
       });
     
 
-// swipe elements swipejs
-// var swiper = new Swiper(".slide-content", {
-//   slidesPerView: 3,
-//   spaceBetween: 25,
-//   centerSlide:'true',
-//   grabCursor:'true',
-//   fade:'true',
-//   loop: true,
-//   loopFillGroupWithBlank: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-//   breakpoints: {
-//     0:{
-//       slidesPerView:1,
-//     },
-//     820:{
-//       slidesPerView:2,
-//     },
-//     950:{
-//     slidesPerView: 3,
-//     },
-//   }
-// });
 
 var swiper = new Swiper(".slide-content", {
   effect: "coverflow",
